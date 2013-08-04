f942c7b569a7f28e7cf6fdf64ffcb0292dab3f64
Allen Nguyen <allen.nguyen@zocdoc.com>
2013-08-03 23:27:43 -0400

    adding web view

--- PouchApp/src/com/example/pouchapp/MainActivity.java
+++ PouchApp/src/com/example/pouchapp/MainActivity.java
@@ -11,12 +11,14 @@ import java.util.Map;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -35,31 +37,23 @@ public class MainActivity extends Activity {
         service = new MemoryService();
         Intent i = new Intent(this, MemoryService.class);
         this.startService(i);
-        /*
-        List<Map<String, String>> planetsList = initFeed();
+
         ListView lv = (ListView) findViewById(R.id.listView);
-        registerForContextMenu(lv);
-        simpleAdpt = new SimpleAdapter(this, planetsList, android.R.layout.simple_list_item_1, new String[] {"planet"}, new int[] {android.R.id.text1});
-        lv.setAdapter(simpleAdpt);
+        List<Feed> feeds = getFeed();
+        ListAdapter adapter = new FeedAdapter(this, feeds, android.R.layout.simple_list_item_1,
+        									   new String[] {Feed.KEY_SOURCE, Feed.KEY_TITLE},
+        									   new int[] {android.R.id.text1, android.R.id.text2});
+        lv.setAdapter(adapter);
         
         // React to user clicks on item
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-             public void onItemClick(AdapterView<?> parentAdapter, View view, int position,
-                                     long id) {
-            	 TextView clickedView = (TextView) view;
-                 renderHtml(clickedView);
-             }
-        });
-        */
-        
-//         ListView lv = (ListView) findViewsById(R.id.listView);
-        ListView lv = (ListView) findViewById(R.id.listView);
-        List<Feed> feeds = getFeed();
-        ListAdapter adapter = new FeedAdapter(this, feeds, android.R.layout.simple_list_item_2,
-        									   new String[] {Feed.KEY_SOURCE, Feed.KEY_TITLE},
-        									   new int[] {android.R.id.text1, android.R.id.text2});
-        lv.setAdapter(adapter);
+            public void onItemClick(AdapterView<?> parentAdapter, View view, int position,
+                                    long id) {
+           	 TextView clickedView = (TextView) view;
+             renderFromSite(clickedView);
+            }
+       });
     }
     
     private List<Map<String, String>> initFeed(){
@@ -76,7 +70,7 @@ public class MainActivity extends Activity {
     
     private List<Feed> getFeed() {
     	List<Feed> feeds = new ArrayList<Feed>();
-    	feeds.add(new Feed("Verge", "Obamba"));
+    	feeds.add(new Feed("Verge", "Obama"));
     	feeds.add(new Feed("Verge", "Michelle"));
     	feeds.add(new Feed("Verge", "Android"));
     	feeds.add(new Feed("Verge", "Apple"));
@@ -91,7 +85,7 @@ public class MainActivity extends Activity {
     	return planet;
     }
     
- // We want to create a context Menu when the user long click on an item
+    // We want to create a context Menu when the user long click on an item
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
             ContextMenuInfo menuInfo) {
@@ -114,7 +108,6 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    
     public void renderHtml(TextView view){
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
     	
@@ -122,7 +115,7 @@ public class MainActivity extends Activity {
 		StringBuilder message = new StringBuilder("");
     	try{
     		AssetManager am = this.getAssets();
-        	InputStream is = am.open(view.getText().toString());
+        	InputStream is = am.open("verge_page_output.html");
     		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
     		while((read = bufferedReader.readLine()) != null){
     			message.append(read);
@@ -137,10 +130,19 @@ public class MainActivity extends Activity {
     	startActivity(intent);
     }
     
+    public void renderFromSite(TextView view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	startActivity(intent);
+    }
+    
+    public void renderFeed(View view) {
+    	Uri uri = Uri.parse("http://example.com/");
+    	Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+    	startActivity(intent);
+    }
     
     public static void main(String[] args) throws Exception{
     	
     	throw new Exception();
     }
-
 }
